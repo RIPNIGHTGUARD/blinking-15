9acc572deb45b9fac7fbf744920c39fa20936ae6
RIPNIGHTGUARD <92817225+RIPNIGHTGUARD@users.noreply.github.com>
2021-10-29 14:15:43 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,91 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . .
+        # . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        . # . . .
+        # # . . .
+        # . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        # . . . .
+        # # . . .
+        # . # . .
+        # # # . .
+        # # . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . . .
+        # # # . .
+        . # . # .
+        # # # # .
+        # # # . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . . .
+        # # # . .
+        . # . # .
+        # # # # .
+        # # # . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        # # # # #
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . # .
+        . . # # #
+        . # . # .
+        . # # # #
+        . . # # #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . # . #
+        . . # # #
+        . . . # #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # .
+        . . . # #
+        . . . . #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . #
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showString("While trick or treating remember to look both ways before crossing the street.")
+})
